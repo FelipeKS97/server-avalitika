4369e84269f1dd73926907eefb61f845fa1b0d36
Felipe Carvalho <felipecavalcanti100@gmail.com>
2020-08-11 15:06:42 -0300

    Adding configuration for deployment on heroku

---
+++ Procfile
@@ -0,0 +1,3 @@
+rollback: ENV_SILENT=true node ace migration:rollback
+release: ENV_SILENT=true node ace migration:run --force
+web: ENV_SILENT=true npm web-start
--- config/database.js
+++ config/database.js
@@ -6,17 +6,31 @@ const Env = use('Env')
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 
+const Url = require('url-parse')
+const NODE_ENV = Env.get('NODE_ENV')
+const CLEARDB_DATABASE_URL = new Url(Env.get('CLEARDB_DATABASE_URL'))
+
 module.exports = {
   /*
   |--------------------------------------------------------------------------
-  | Default Connection
+  | Default/Custom Connection
   |--------------------------------------------------------------------------
   |
   | Connection defines the default connection settings to be used while
   | interacting with SQL databases.
+  | 
+  | This connection was customized to use ClearDB MySql environment variables
+  | configured by heroku for deployment.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'mysql'),
+  // connection: Env.get('DB_CONNECTION', 'mysql'),
+  connection: NODE_ENV === 'production' ?  {
+    host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
+    port: Env.get('DB_PORT', ''),
+    user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
+    password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
+    database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
+  } : Env.get('DB_CONNECTION', 'mysql'), 
 
   /*
   |--------------------------------------------------------------------------
